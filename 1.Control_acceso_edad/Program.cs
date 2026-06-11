289722b6a228a4b1496e7b1a79c3efbaec5d36e6
Natanael Mejía <2024-0377@unad.edu.do>
2026-06-11 01:07:05 -0400

    Add files via upload

---
+++ 1.Control_acceso_edad/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Ejercicio 1. Control de acceso por edad
+Un sistema de seguridad automatizado debe validar la edad de un usuario al intentar ingresar a un establecimiento.
+Si la persona tiene 18 años o más, el sistema le permite al acceso mostrar un mensaje de bienvenida; 
+en caso contrario, deniega la entrada con un mensaje de restricción.*/
+
+using System;
+
+class ControlAcceso
+{
+    static void Main()
+    {
+        Console.Write("Ingrese su edad:");
+        int edad = int.Parse(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Acceso permitido. ¡Bienvenido!");
+        }
+        else
+        {
+            Console.WriteLine("Acceso denegado. Debes ser mayor de edad.");
+        }
+    }
+}
