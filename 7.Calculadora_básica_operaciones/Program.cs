cd61531383a76642a77d94ac9a111900a9ae5ff9
Natanael Mejía <2024-0377@unad.edu.do>
2026-06-11 01:14:04 -0400

    Add files via upload

---
+++ 7.Calculadora_básica_operaciones/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Ejercicio 7. Calculadora básica de operaciones
+Un usuario ingresa dos números y un símbolo de operación matemática ( +, -, *, /). 
+El programa evalúa el símbolo seleccionado para ejecutar exclusivamente la operación 
+correspondiente entre ambos números y muestra el resultado en pantalla.*/
+
+using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer numero:");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese uno de estos operador (+, -, *, /):");
+        string op = Console.ReadLine();
+
+        Console.Write("Ingrese el segundo numero:");
+        double b = double.Parse(Console.ReadLine());
+
+        switch (op)
+        {
+            case "+":
+                Console.WriteLine($"Resultado:{a + b}");
+                break;
+            case "-":
+                Console.WriteLine($"Resultado:{a - b}");
+                break;
+            case "*":
+                Console.WriteLine($"Resultado:{a * b}");
+                break;
+            case "/":
+                if (b == 0)
+                    Console.WriteLine("!Error! No se puede dividir entre cero.");
+                else
+                    Console.WriteLine($"Resultado:{a / b}");
+                break;
+            default:
+                Console.WriteLine("!Operador no válido!");
+                break;
+        }
+    }
+}
---
+++ 8.Conversor_tipo_usuario/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Ejercicio 8. Conversor de tipo de usuario (Roles de sistema)
+Una plataforma web lee el tipo de rol de un usuario mediante una letra clave 
+( A para Administrador, E para Editor, A para Autor, L para Lector). 
+Dependiendo de la letra, el sistema desplegará en pantalla los permisos específicos asignados a ese rol. 
+Si la letra no coincide, se asigna el rol de "Invitado".*/
+
+using System;
+
+class Roles
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la letra del rol (A/E/U/L):");
+        string input = Console.ReadLine().ToUpper();
+        char rol = input[0];
+
+        switch (rol)
+        {
+            case 'A':
+                Console.WriteLine("Rol: !Administrador!");
+                Console.WriteLine("Permisos: Acceso total al sistema.");
+                break;
+            case 'E':
+                Console.WriteLine("Rol: !Editor!");
+                Console.WriteLine("Permisos: Crear, editar y publicar contenido.");
+                break;
+            case 'U':
+                Console.WriteLine("Rol: !Autor!");
+                Console.WriteLine("Permisos: Crear y editar su propio contenido.");
+                break;
+            case 'L':
+                Console.WriteLine("Rol: !Lector!");
+                Console.WriteLine("Permisos: Solo lectura.");
+                break;
+            default:
+                Console.WriteLine("Rol: !Invitado!");
+                Console.WriteLine("Permisos: Acceso restringido.");
+                break;
+        }
+    }
+}
