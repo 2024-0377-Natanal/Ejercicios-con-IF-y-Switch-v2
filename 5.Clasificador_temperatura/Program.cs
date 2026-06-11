2c625051b344a235204b47b4bf3fdbc55c485718
Natanael Mejía <2024-0377@unad.edu.do>
2026-06-11 01:12:41 -0400

    Add files via upload

---
+++ 5.Clasificador_temperatura/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Ejercicio 5. Clasificador de temperatura ambiental
+Un dispositivo meteorológico toma la temperatura actual en grados Celsius. 
+Si la temperatura es estrictamente menor a 15°C, el sistema activa la alerta de "Clima Frío"; 
+si la temperatura está entre 15°C y 28°C inclusive, muestra "Clima Templado"; y si supera los 28°C, 
+activa la alerta de "Clima Cálido".*/
+
+using System;
+
+class Temperatura
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la temperatura en °C:");
+        double temp = double.Parse(Console.ReadLine());
+
+        if (temp < 15)
+        {
+            Console.WriteLine("!Alerta! Clima Frío");
+        }
+        else if (temp >= 15 && temp <= 28)
+        {
+            Console.WriteLine("Clima Templado");
+        }
+        else
+        {
+            Console.WriteLine("!Alerta! Clima Cálido");
+        }
+    }
+}
---
+++ 6. Selector_días_emana/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Ejercicio 6. Selector de días de la semana
+Un calendario digital recibe un número entero del 1 al 7 introducido por el usuario. 
+El programa traduce ese número al nombre del día correspondiente (donde 1 es Lunes y 7 es Domingo). 
+Si el usuario ingresa un número fuera de ese rango, el sistema muestra un mensaje indicando que el día es inválido.*/
+
+using System;
+
+class DiasSemana
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número del 1 al 7:");
+        int dia = int.Parse(Console.ReadLine());
+
+        switch (dia)
+        {
+            case 1: Console.WriteLine("!Lunes!"); break;
+            case 2: Console.WriteLine("!Martes!"); break;
+            case 3: Console.WriteLine("!Miércoles!"); break;
+            case 4: Console.WriteLine("!Jueves!"); break;
+            case 5: Console.WriteLine("!Viernes!"); break;
+            case 6: Console.WriteLine("!Sábado!"); break;
+            case 7: Console.WriteLine("!Domingo!"); break;
+            default: Console.WriteLine("Día inválido"); break;
+        }
+    }
+}
