2f52f0c59827380a0af0581eed4d4970535605ff
Natanael Mejía <2024-0377@unad.edu.do>
2026-06-11 01:10:46 -0400

    Add files via upload

---
+++ 3.Verificador_números_pares_impares/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Ejercicio 3. Verificador de números pares e impares
+Una herramienta de análisis matemático recibe un número entero por parte del usuario. 
+El programa determina si dicho número es divisible exactamente entre dos, clasificándolo y 
+mostrándolo como "Número Par",o como "Número Impar" si tiene un residuo.*/
+
+using System;
+
+class ParImpar
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número entero:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} es un Numero Par.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} es un Numero Impar.");
+        }
+    }
+}
